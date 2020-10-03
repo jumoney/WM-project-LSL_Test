74a7fe2d55b6eea58f52f294bcfe3ab54c5146f2
jumoney <jumoney15@naver.com>
2020-10-03 22:36:51 +0900

    2020-10-03 3d전시관 db자료 출력까지 완료

--- WM_Test/src/com/lsl/wm/gallay_3d/Gallay3dSer.java
+++ WM_Test/src/com/lsl/wm/gallay_3d/Gallay3dSer.java
@@ -1,11 +1,22 @@
 package com.lsl.wm.gallay_3d;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.lsl.wm.MyUtils;
+import com.lsl.wm.db.ShowDAO;
+import com.lsl.wm.db.ShowListDAO;
+import com.lsl.wm.vo.ShowListDomain;
+import com.lsl.wm.vo.ShowListVO;
+import com.lsl.wm.vo.ShowVO;
+import com.lsl.wm.vo.UserVO;
 
 
 @WebServlet("/gallay/gallay3d")
@@ -13,6 +24,24 @@ public class Gallay3dSer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//로그인한 사용자 정보를 받아온다.
+		UserVO loginUser = MyUtils.getLoginUser(request);
+		//전시회 정보를 받아온다.
+		ShowVO param = ShowDAO.selLatestExhibition();
+		//전시회 작품 목록을 가져온다.
+		ShowListVO vo = new ShowListVO();
+		
+		vo.setI_show(param.getI_show());
+		List<ShowListDomain> list = new ArrayList();
+		list = ShowListDAO.selShowList(vo);
+		
+		String savePath2 = "/resource/user_writer/images/exhibition/";
+		
+		//전시회 작품 목록을 보내준다.
+		request.setAttribute("workList", list);
+		//jsp에서 출력해줄 이미지의 경로를 보내준다.
+		request.setAttribute("workImagePath", savePath2);
+		
 		String jsp = "/WEB-INF/3d_gallay/3dGallay.jsp"; 
 		request.getRequestDispatcher(jsp).forward(request, response);
 	}
